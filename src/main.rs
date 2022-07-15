e78973a06581567ef198734347ddbc843543a937
twcrone <twcrone@gmail.com>
2022-07-15 16:41:38 -0400

    Add base logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("am")
+    let matches = App::new("am")
         .version("0.1.0")
         .author("Todd W Crone <twcrone@gmail.com>")
         .about("Alias Manager")
@@ -19,5 +19,19 @@ fn main() {
         )
         .get_matches();
 
-    println!("{:#?}", _matches)
+    let alias_option = matches.value_of_lossy("alias");
+    if alias_option.is_some() {
+        let alias = alias_option.unwrap();
+        let delete = matches.is_present("delete");
+        if delete {
+            print!("delete ")
+        }
+        else {
+            print!("add/update ")
+        }
+        println!("{}", alias)
+    }
+    else {
+        println!("list aliases")
+    }
 }
