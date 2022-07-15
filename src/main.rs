1f8befefd2db6dc4af8a0af874d8be1791567c66
twcrone <twcrone@gmail.com>
2022-07-15 16:19:23 -0400

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-    let _matches = App::new("ram")
+    let _matches = App::new("am")
         .version("0.1.0")
         .author("Todd W Crone <twcrone@gmail.com>")
         .about("Alias Manager")
+        .arg(
+            Arg::with_name("alias")
+                .value_name("ALIAS")
+                .help("Alias name")
+                .required(true)
+                .max_values(1),
+        )
+        .arg(
+            Arg::with_name("delete")
+                .short("d")
+                .help("Delete the alias")
+                .takes_value(false),
+        )
         .get_matches();
+
+    println!("{:#?}", _matches)
 }
